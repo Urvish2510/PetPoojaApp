e23427c23ca53be7f6ecae1883edce63112a8751
Urvish Patel <uvpatel2510@gmail.com>
2021-11-23 14:12:04 +0530

    Completed basic google auth API setup

--- server/index.js
+++ server/index.js
@@ -2,17 +2,26 @@ require('dotenv').config();
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
+import passport from 'passport';
 
 // Database Connection
 import ConnectDB from './database/connection';
 
+// google authentication config
+import googleAuthConfig from './config/google.config';
+
 // API
 import Auth from './API/Auth';
+
+//passport config
+googleAuthConfig(passport);
 
 const petpooja = express();
 petpooja.use(cors());
 petpooja.use(express.json());
 petpooja.use(helmet());
+petpooja.use(passport.initialize());
+petpooja.use(passport.session());
 
 // Application Routes
 petpooja.use("/auth", Auth)
