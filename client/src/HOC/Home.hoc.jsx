0bf76157732b34bf29e5ef4c55dd584cad76de00
Urvish Patel <uvpatel2510@gmail.com>
2021-12-07 20:28:12 +0530

    Created delivery, restaurant and nutrition part

--- client/src/HOC/Home.hoc.jsx
+++ client/src/HOC/Home.hoc.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Route } from "react-router-dom";
 
 // Layout
-import HomeLayout from "../layouts/Homepage.layout";
+import HomeLayout from "../Layouts/Homepage.layout.jsx";
 
 function HomeLayoutHoc({ component: Component, ...rest }) {
   return (
---
+++ client/src/Layouts/Homepage.layout.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../Components/Navbar";
+import FoodTab from "../Components/FoodTab";
+
+function HomeLayout({ children }) {
+  return (
+    <div>
+      <Navbar />
+      <FoodTab />
+      <div className="container mx-auto px-4 lg:px-20">{children}</div>
+    </div>
+  );
+}
+
+export default HomeLayout;
